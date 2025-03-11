ee7cc39967293145064e2c8aedf86af411468b96
DatPirate <116280939+DatPirate@users.noreply.github.com>
2025-03-11 16:33:40 +0300

    update: реализована базовая обработка POST-запросов (SRDV-3)

--- server/server.py
+++ server/server.py
@@ -1,3 +1,24 @@
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+ 
+class CalcHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+    	try:
+		output = "request recieved"
+		self.send_response(200)
+		self.send_header("Content-Type", "application/json")
+		self.end_headers()
+		self.wfile.write(json.dumps(output).encode("utf-8"))
+	except Exception as e:
+            	self.send_response(500)
+            	self.send_header("Content-Type", "application/json")
+            	self.end_headers()
+            	self.wfile.write(json.dumps({"error": str(e)}).encode("utf-8"))
+ 
 if __name__ == "__main__":
-    print("server")
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, CalcHandler)
+    print("Starting server on port 8000...")
+    httpd.serve_forever()
  
